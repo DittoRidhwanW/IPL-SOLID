ac15e6382a3dac8b8ecbdf81144414c571faf762
dittoRW <147579048+dittoRW@users.noreply.github.com>
2023-10-22 15:36:47 +0700

    Update Class Standard Cinema

--- OpenClose/StandardCinema.java
+++ OpenClose/StandardCinema.java
@@ -2,4 +2,8 @@ class StandardCinema extends Cinema {
     public StandardCinema(double price) {
         this.price = price;
     }
+    @Override
+    Double calculateAdminFee() {
+    return price * 10 / 100;
+    }
 }
